09cb127491a8c7b3dc587f7741e5418786a39065
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-08-22 01:31:11 -0400

    Added dynamic port support to server.js with default port being 3500; built HTML using Bootstrap for home.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 // Initialize Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3500;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
