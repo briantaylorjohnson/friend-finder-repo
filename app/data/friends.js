f643f901dba12be727ffb71bff1ec93221fe1ff1
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-08-27 01:58:44 -0400

    Refreshed data in friends.js to add more potential friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,18 +2,18 @@
 var friends = [
   {
     "name": "Road Runner",
-    "image": "https://vignette.wikia.nocookie.net/wbau/images/1/13/Looneytunes-roadrunner.gif",
+    "image": "https://i.ebayimg.com/images/g/dSQAAOSwP8db72eA/s-l300.jpg",
     "scores": [
-        2,
-        3,
-        5,
         1,
-        3,
         2,
+        3,
         4,
         5,
+        5,
+        4,
         3,
-        4
+        2,
+        1
     ]
   },
 
@@ -21,35 +21,171 @@ var friends = [
       "name": "Daffy Duck",
       "image": "https://vignette.wikia.nocookie.net/looneytunes/images/8/88/Daffy-1-.jpg",
       "scores": [
-          5,
+          2,
           2,
           3,
-          1,
-          1,
-          1,
           3,
           4,
-          3,
+          4,
+          5,
+          5,
+          1,
           1
       ]
     },
 
-    {
-      "name": "Pooh Bear",
-      "image": "https://www.yourwdwstore.net/assets/images/pins/2010pins/09sept/400000479019.jpg",
+  {
+    "name": "Bugs Bunny",
+    "image": "https://vignette.wikia.nocookie.net/looneytunes/images/2/26/Classic_bugsbunny.png",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        4,
+        2,
+        2,
+        2
+    ]
+  },
+
+  {
+    "name": "Wile E. Coyote",
+    "image": "https://vignette.wikia.nocookie.net/looneytunes/images/7/7f/Coyote.gif/revision/latest?cb=20060219181853",
+    "scores": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+  },
+
+  {
+      "name": "Yosemite Sam",
+      "image": "https://vignette.wikia.nocookie.net/looneytunes/images/8/8c/Yosemite_Sam.png/revision/latest/scale-to-width-down/250?cb=20180523161541",
       "scores": [
+          3,
+          2,
+          1,
+          5,
           4,
           3,
-          4,
-          3,
           2,
-          3,
-          2,
-          3,
-          2,
-          1
+          1,
+          5,
+          5
       ]
-    }
+    },
+
+    {
+      "name": "Tweety",
+      "image": "https://vignette.wikia.nocookie.net/looneytunes/images/e/e7/Tweety1.gif/revision/latest/scale-to-width-down/250?cb=20140406233615",
+      "scores": [
+          2,
+          3,
+          1,
+          5,
+          1,
+          2,
+          2,
+          1,
+          1,
+          3
+      ]
+    },
+
+    {
+      "name": "Porky Pig",
+      "image": "https://vignette.wikia.nocookie.net/looneytunes/images/f/fc/Porky_Pig.gif/revision/latest?cb=20160417194145",
+      "scores": [
+          1,
+          1,
+          5,
+          5,
+          3,
+          3,
+          2,
+          2,
+          4,
+          4
+      ]
+    },
+
+    {
+        "name": "Daffy Duck",
+        "image": "https://vignette.wikia.nocookie.net/looneytunes/images/8/88/Daffy-1-.jpg/revision/latest?cb=20130825180446",
+        "scores": [
+            5,
+            4,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+      },
+
+      {
+        "name": "Tazmanian Devil",
+        "image": "https://vignette.wikia.nocookie.net/looneytunes/images/e/e1/230px-Taz-Looney_Tunes_svg.png/revision/latest?cb=20130825180200",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            2,
+            2,
+            5,
+            3,
+            1
+        ]
+      },
+
+      {
+        "name": "Pepe Le Pew",
+        "image": "https://vignette.wikia.nocookie.net/looneytunes/images/9/91/Pepe-Le-Pew.png/revision/latest/scale-to-width-down/250?cb=20190108223946",
+        "scores": [
+            4,
+            4,
+            3,
+            3,
+            1,
+            1,
+            2,
+            2,
+            3,
+            4
+        ]
+      },
+    
+      {
+          "name": "Sylvester",
+          "image": "https://vignette.wikia.nocookie.net/looneytunes/images/8/88/Sylvester_the_Cat.png/revision/latest?cb=20130119144206",
+          "scores": [
+              5,
+              2,
+              3,
+              1,
+              1,
+              1,
+              3,
+              4,
+              3,
+              1
+          ]
+        }
 ];
 
 module.exports = friends;
